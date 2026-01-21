f7b3e022245ec6b59f7229e8262f139b8e52532a
SPIDERBOYOMG <imangalierden2000@gmail.com>
2026-01-21 11:51:07 +0500

    numbers

---
+++ python-basics/numbers.py
@@ -0,0 +1,9 @@
+#nubmers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#squares
+squares = [n**2 for n in numbers]
+#cubes
+cubes = [n**3 for n in numbers]
+print("Numbers: ", numbers)
+print("Squares: ", squares)
+print("Cubes: ", cubes)
