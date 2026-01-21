9970bbe360b113208924a97c751c5a248c3429b7
SPIDERBOYOMG <imangalierden2000@gmail.com>
2026-01-21 11:52:17 +0500

    casting

---
+++ python-basics/casting.py
@@ -0,0 +1,14 @@
+#casting
+a=10
+b=20.5
+print(type(a), a)
+print(type(b), b)
+a=float(a)
+b=int(b)
+print(type(a), a)
+print(type(b), b)
+#input
+c=input("Enter a number:")
+print(type(c), c)
+c=int(c)
+print(type(c), c)
