ec34ed74f92c3e56aa9e288104c2f092cd2c14d1
SPIDERBOYOMG <imangalierden2000@gmail.com>
2026-01-21 11:48:39 +0500

    data_types

---
+++ python-basics/data_types.py
@@ -0,0 +1,9 @@
+#data types
+a = 10          # integer
+b = 3.14        # float
+c = "Hello"     # string
+d = True        # boolean
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+print(type(d), d)
