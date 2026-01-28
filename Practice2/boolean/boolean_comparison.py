b14ffac8ddeb1e3e4fb1416589b3ad554f496257
SPIDERBOYOMG <imangalierden2000@gmail.com>
2026-01-28 19:34:33 +0500

    added comparison.py

---
+++ Practice2/boolean/boolean_comparison.py
@@ -0,0 +1,10 @@
+#boolean_comparison.py
+a = 10
+b = 20
+print("Is a equal to b?", a == b)
+print("Is a not equal to b?", a != b)
+print("Is a greater than b?", a > b)
+print("Is a less than b?", a < b)
+print("Is a greater than or equal to b?", a >= b)
+print("Is a less than or equal to b?", a <= b)
+# This code demonstrates comparison operations in Python.
