1168bf4ff056ae279e41419ce0b090fc605b7835
SPIDERBOYOMG <imangalierden2000@gmail.com>
2026-01-28 19:31:06 +0500

    added practice2 folder

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,10 @@
+#boolean_into.py
+is_raining = True
+is_sunny = False
+print("Is it raining?", is_raining)
+print("Is it sunny?", is_sunny)
+print("Is it both raining and sunny?", is_raining and is_sunny)
+print("Is it either raining or sunny?", is_raining or is_sunny)
+print("Is it not raining?", not is_raining)
+print("Is it not sunny?", not is_sunny)
+# This code demonstrates basic boolean operations in Python.
